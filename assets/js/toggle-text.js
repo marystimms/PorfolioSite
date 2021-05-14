c9ffaf5282d0c76a686f1d75ae18eeb274315adc
marystimms <mary.timms1@googlemail.com>
2021-05-14 08:07:23 +0100

    add footer, bug fixes

--- assets/js/toggle-text.js
+++ assets/js/toggle-text.js
@@ -1,12 +1,64 @@
 
-function toggleCardText(textID, buttonID) {
-    var x = document.getElementById(textID);
-    var y = document.getElementById(buttonID);
-    if (x.style.display === 'none') {
-        x.style.display = "block";
-        y.innerHTML = "See Less"
-    } else{
-        x.style.display = "none";
-        y.innerHTML = "See More"
-    }
-}
+function collapseSection(element) {
+    // get the height of the element's inner content, regardless of its actual size
+    var sectionHeight = element.scrollHeight;
+    
+    // temporarily disable all css transitions
+    var elementTransition = element.style.transition;
+    element.style.transition = '';
+    
+    // on the next frame (as soon as the previous style change has taken effect),
+    // explicitly set the element's height to its current pixel height, so we 
+    // aren't transitioning out of 'auto'
+    requestAnimationFrame(function() {
+      element.style.height = sectionHeight + 'px';
+      element.style.transition = elementTransition;
+      
+      // on the next frame (as soon as the previous style change has taken effect),
+      // have the element transition to height: 0
+      requestAnimationFrame(function() {
+        element.style.height = 0 + 'px';
+      });
+    });
+    
+    // mark the section as "currently collapsed"
+    element.setAttribute('data-collapsed', 'true');
+  }
+  
+  function expandSection(element) {
+    // get the height of the element's inner content, regardless of its actual size
+    var sectionHeight = element.scrollHeight;
+    
+    // have the element transition to the height of its inner content
+    element.style.height = sectionHeight + 'px';
+  
+    // when the next css transition finishes (which should be the one we just triggered)
+    element.addEventListener('transitionend', function(e) {
+      // remove this event listener so it only gets triggered once
+      element.removeEventListener('transitionend', arguments.callee);
+      
+      // remove "height" from the element's inline styles, so it can return to its initial value
+      element.style.height = null;
+    });
+    
+    // mark the section as "currently not collapsed"
+    element.setAttribute('data-collapsed', 'false');
+  }
+
+  collapseSection(document.getElementById('loreto'));
+  collapseSection(document.getElementById('durham'));
+  collapseSection(document.getElementById('justit'));
+  
+
+
+  function toggleText(button, textId){
+    var section = document.getElementById(textId);
+    var isCollapsed = section.getAttribute('data-collapsed') === 'true';
+    console.log(isCollapsed, section);
+    if(isCollapsed) {
+        expandSection(section)
+        section.setAttribute('data-collapsed', 'false')
+      } else {
+        collapseSection(section)
+      }
+  };
